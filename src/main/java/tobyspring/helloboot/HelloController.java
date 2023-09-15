1b415112b890ebd872c544052c3b8efcdd6a14d7
ej <hanej93@naver.com>
2023-09-15 19:01:43 +0900

    의존 오브젝트 추가

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -3,10 +3,14 @@ package tobyspring.helloboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 public class HelloController {
 	@GetMapping("/hello")
 	public String hello(String name) {
-		return "Hello" + name;
+		SimpleHelloService helloService = new SimpleHelloService();
+
+		return helloService.sayHello(Objects.requireNonNull(name));
 	}
 }
