b3d507467f057e39f0ebfe7d280549cddc6291dd
ej <hanej93@naver.com>
2023-09-18 18:32:59 +0900

    메타 애노테이션과 합성 애노테이션

--- src/test/java/tobyspring/helloboot/HelloServiceTest.java
+++ src/test/java/tobyspring/helloboot/HelloServiceTest.java
@@ -1,8 +1,8 @@
 package tobyspring.helloboot;
 
-import static org.assertj.core.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class HelloServiceTest {
 
