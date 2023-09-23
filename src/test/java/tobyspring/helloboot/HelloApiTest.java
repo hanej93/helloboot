aa15333f2423ed40cf16a261702699839e6d90df
EJ <hanej93@naver.com>
2023-09-23 21:18:18 +0900

    자동 구성에 Environment 프로퍼티 적용

--- src/test/java/tobyspring/helloboot/HelloApiTest.java
+++ src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -9,14 +9,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
-public class HelloApiTest {
+class HelloApiTest {
 
 	@Test
 	void helloApi() {
 		TestRestTemplate rest = new TestRestTemplate();
 
 		ResponseEntity<String> res =
-			rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+			rest.getForEntity("http://localhost:8080/app/hello?name={name}", String.class, "Spring");
 
 		// status code 200
 		assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
@@ -31,7 +31,7 @@ public class HelloApiTest {
 		TestRestTemplate rest = new TestRestTemplate();
 
 		ResponseEntity<String> res =
-			rest.getForEntity("http://localhost:8080/hello?name=", String.class);
+			rest.getForEntity("http://localhost:8080/app/hello?name=", String.class);
 
 		assertThat(res.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
