ce5a3f39a2f640b181d36a1bddb66cc9cdd93005
ej <hanej93@naver.com>
2023-10-04 19:08:37 +0900

    Hello 리포지토리

--- src/test/java/tobyspring/helloboot/JdbcTemplateTest.java
+++ src/test/java/tobyspring/helloboot/JdbcTemplateTest.java
@@ -1,10 +1,11 @@
 package tobyspring.helloboot;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @HelloBootTest
 class JdbcTemplateTest {
@@ -22,6 +23,6 @@ class JdbcTemplateTest {
 		jdbcTemplate.update("insert into hello values (?, ?)", "Spring", 1);
 
 		Long count = jdbcTemplate.queryForObject("select count(*) from hello", Long.class);
-		Assertions.assertThat(count).isEqualTo(2);
+		assertThat(count).isEqualTo(2);
 	}
 }
